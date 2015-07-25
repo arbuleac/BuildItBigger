d2c6541ad432da4958480952132f94dd520ea5fb
arbuleac <dev@arbuleac.com>
2015-07-25 19:04:59 +0300

    JokeProbider library

---
+++ jokeprovider/src/main/java/com/arbuleac/jokeprovider/JokeProvider.java
@@ -0,0 +1,24 @@
+package com.arbuleac.jokeprovider;
+
+public class JokeProvider {
+
+    private static JokeProvider sInstance;
+
+    public static JokeProvider get() {
+        if (sInstance == null) {
+            synchronized (JokeProvider.class) {
+                if (sInstance == null) {
+                    sInstance = new JokeProvider();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    public String next() {
+        return "Q: how many programmers does it take to change a light bulb?" +
+                "\n" +
+                "A: none, that's a hardware problem";
+    }
+
+}
